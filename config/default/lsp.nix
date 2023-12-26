c5d9dbf4d571844fc2a0572ce4d97811cacf0c9f
asungy <62207329+asungy@users.noreply.github.com>
2023-12-26 10:44:36 -0500

    Added tsserver

--- config/default/lsp.nix
+++ config/default/lsp.nix
@@ -4,6 +4,7 @@
     servers = {
       nixd.enable = true;
       terraformls.enable = true;
+      tsserver.enable = true;
     };
     preConfig = builtins.readFile ../_common/lsp_preconfig.lua;
   };
