a7ad9893efd9564e4b7d5b766d30419c146c399a
asungy <62207329+asungy@users.noreply.github.com>
2024-06-09 09:18:34 -0400

    Added templ to go output

--- config/go/default.nix
+++ config/go/default.nix
@@ -6,6 +6,7 @@
       lsp = default.plugins.lsp //
       {
         servers.gopls.enable = true;
+        servers.templ.enable = true;
       };
     };
     extraPackages = with pkgs; default.extraPackages
