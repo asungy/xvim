134422ba7e86cdc89ecf27a27fcf39ef7d0bafc2
asungy <62207329+asungy@users.noreply.github.com>
2024-06-13 14:37:31 -0400

    Added harpoon keymaps

--- config/default/plugins/harpoon.nix
+++ config/default/plugins/harpoon.nix
@@ -4,7 +4,21 @@
     enableTelescope = true;
     keymaps = {
       addFile = "<leader>ha";
-      toggleQuickMenu = "<leader>ht";
+      toggleQuickMenu = "<leader>ho";
+      navPrev = "<leader>he";
+      navNext = "<leader>hu";
+      gotoTerminal = {
+        "1" = "<leader>h<S-h>";
+        "2" = "<leader>h<S-t>";
+        "3" = "<leader>h<S-n>";
+        "4" = "<leader>h<S-s>";
+      };
+      navFile = {
+        "1" = "<leader>hh";
+        "2" = "<leader>ht";
+        "3" = "<leader>hn";
+        "4" = "<leader>hs";
+      };
     };
   };
 }
