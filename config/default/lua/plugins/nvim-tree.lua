5f2a9dea86423dc013f91231b59bcf7e1d4153b6
asungy <62207329+asungy@users.noreply.github.com>
2024-09-10 12:05:50 -0400

    Fixed nvim tree issue with zen mode

--- config/default/lua/plugins/nvim-tree.lua
+++ config/default/lua/plugins/nvim-tree.lua
@@ -6,9 +6,15 @@ do
   local WIDTH_RATIO = 0.5
   require("nvim-tree").setup({
     view = {
+      open_file = {
+        window_picker = {
+          enable = false,
+        },
+      },
       float = {
         enable = true,
         quit_on_focus_loss = true,
+        hijack_unnamed_buffer_when_opening = true,
         -- Make the neovim window appear in the center of the screen
         open_win_config = function()
           local screen_w = vim.opt.columns:get()
