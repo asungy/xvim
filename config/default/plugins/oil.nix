3cc66e88ab80660f82efe4bb9ef3fc83e6eae0b3
asungy <62207329+asungy@users.noreply.github.com>
2024-09-17 22:13:00 -0400

    Added keymaps to oil settings

--- config/default/plugins/oil.nix
+++ config/default/plugins/oil.nix
@@ -1,5 +1,18 @@
+let
+  leader = "=";
+in
 {
   oil = {
     enable = true;
+    settings = {
+      keymaps = {
+        ${ leader + "." } = "actions.toggle_hidden";
+        ${ leader + "h" } = "actions.show_help";
+        ${ leader + "p" } = "actions.preview";
+        ${ leader + "q" } = "actions.close";
+        ${ leader + "r" } = "actions.refresh";
+        ${ leader + "y" } = "actions.yank_entry";
+      };
+    };
   };
 }
